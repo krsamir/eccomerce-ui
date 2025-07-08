820f9058c23f2e9887686629a302d9996e3edb73
Amruta24818 <patilamrutaj@gmail.com>
2025-07-08 22:42:12 +0530

    Created UI for managing locations

--- ecom-app/src/components/SuperAdmin/Location/index.jsx
+++ ecom-app/src/components/SuperAdmin/Location/index.jsx
@@ -1,11 +1,69 @@
-import React from "react";
+import React, { useState } from 'react'
+import { AgGridReact } from 'ag-grid-react'
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community'
+import EditIcon from '@mui/icons-material/Edit'
+import DeleteIcon from '@mui/icons-material/Delete'
+import { Button } from '@ecom/ui'
+
+ModuleRegistry.registerModules([AllCommunityModule])
 
 function Location() {
+  // Row Data: The data to be displayed.
+  const [rowData] = useState([
+    {
+      name: 'Lalganj',
+      city: 'Vaishali',
+      state: 'Bihar',
+      country: 'Bharat',
+      created_At: '2025-07-04',
+      updated_At: '2025-07-04',
+    },
+    {
+      name: 'Dumri',
+      city: 'Buxar',
+      state: 'Bihar',
+      country: 'Bharat',
+      created_At: '2025-07-04',
+      updated_At: '2025-07-04',
+    },
+  ])
+
+  // Column Definitions: Defines the columns to be displayed.
+  const [colDefs] = useState([
+    {
+      field: 'action',
+      minWidth: 100,
+      maxWidth: 150,
+      cellStyle: {alignContent:'center'},
+      cellRenderer: () => (
+        <div style={{ display: 'flex', gap: '30px', justifyItems: 'center'}}>
+          <DeleteIcon style={{ cursor: 'pointer', color: 'red' }} />
+          <EditIcon style={{ cursor: 'pointer', color: 'green' }} />
+        </div>
+      ),
+    },
+    { field: 'name' },
+    { field: 'city' },
+    { field: 'state' },
+    { field: 'country' },
+    { field: 'created_At' },
+    { field: 'updated_At' },
+  ])
+
   return (
     <div>
-      <h2>Location</h2>
+      <div style={{ margin: '20px 0 30px 10px' }}>
+        <Button children="Create" />
+      </div>
+      <div style={{ width: '80vw', height: '80vh' }}>
+        <AgGridReact rowData={rowData} columnDefs={colDefs} />
+      </div>
     </div>
-  );
+  )
 }
 
-export default Location;
+export default Location
+
+// ag Grid
+// tanstack
+//
